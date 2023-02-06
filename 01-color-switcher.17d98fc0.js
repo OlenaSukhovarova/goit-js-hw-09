1cff5277d37541475244319667417aa8a9c5ff2a
OlenaSukhovarova <114983170+OlenaSukhovarova@users.noreply.github.com>
2023-02-06 16:15:45 +0000

    Deploying to gh-pages from @ OlenaSukhovarova/goit-js-hw-09@2c32aca3dc897b39c115b275adaf05e8479b8b50 🚀

---
+++ 01-color-switcher.17d98fc0.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;function r(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){o=setInterval(r,1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.17d98fc0.js.map
